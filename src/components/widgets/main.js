cf3892eaa1a4d96318c89a7e4296f4abd319b1af
javi_sosa <jcjavier.sosa@gmail.com>
2022-11-19 18:47:01 -0300

    change structure to send email

--- src/components/widgets/main.js
+++ src/components/widgets/main.js
@@ -1,8 +1,18 @@
-document.getElementById('contact-form').addEventListener('submit', submitForm);
+document.getElementById('contact-form').addEventListener('submit', sendEmail);
 
 // Send Email Info
-function sendEmail(email, message) {
+function sendEmail(e) {
+  e.preventDefault();
+
+  //   Get input Values
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+  console.log(email, message);
+
+  // (<HTMLFormElement>document.querySelector('.contact-form')).reset();
+
   console.log(typeof window.Email);
+
   Email.send({
     Host: 'smtp.gmail.com',
     Username: 'jcarlos.sosagomez@gmail.com',
@@ -14,19 +24,22 @@ function sendEmail(email, message) {
     Subject: `You have a new message`,
     Body: `${message}`,
   }).then((message) => alert('Mail sent successfully!'));
-}
-
-function submitForm(e) {
-  e.preventDefault();
-
-  //   Get input Values
-  let email = document.getElementById('email').value;
-  let message = document.getElementById('message').value;
-
-  // (<HTMLFormElement>document.querySelector('.contact-form')).reset();
 
   document.getElementById('email').value = '';
   document.getElementById('message').value = '';
-
-  sendEmail(email, message);
 }
+
+// function submitForm(e) {
+//   e.preventDefault();
+
+//   //   Get input Values
+//   let email = document.getElementById('email').value;
+//   let message = document.getElementById('message').value;
+
+//   // (<HTMLFormElement>document.querySelector('.contact-form')).reset();
+
+//   document.getElementById('email').value = '';
+//   document.getElementById('message').value = '';
+
+//   sendEmail(email, message);
+// }
