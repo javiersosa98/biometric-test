6e450c56ed0bd125249fc65fab2401b5971be3e0
javi_sosa <jcjavier.sosa@gmail.com>
2022-11-21 00:22:07 -0300

    email service wip

--- src/components/widgets/main.js
+++ src/components/widgets/main.js
@@ -13,16 +13,56 @@ function sendEmail(e) {
 
   console.log(typeof window.Email);
 
-  Email.send({
-    Host: 'smtp.gmail.com',
-    Username: 'jcarlos.sosagomez@gmail.com',
-    Password: 'pkctvjpnsqoskvat',
+  var Email = {
+    send: function (a) {
+      return new Promise(function (n, e) {
+        (a.nocache = Math.floor(1e6 * Math.random() + 1)), (a.Action = 'Send');
+        var t = JSON.stringify(a);
+        Email.ajaxPost('https://smtpjs.com/v3/smtpjs.aspx?', t, function (e) {
+          n(e);
+        });
+      });
+    },
+    ajaxPost: function (e, n, t) {
+      var a = Email.createCORSRequest('POST', e);
+      a.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'),
+        (a.onload = function () {
+          var e = a.responseText;
+          null != t && t(e);
+        }),
+        a.send(n);
+    },
+    ajax: function (e, n) {
+      var t = Email.createCORSRequest('GET', e);
+      (t.onload = function () {
+        var e = t.responseText;
+        null != n && n(e);
+      }),
+        t.send();
+    },
+    createCORSRequest: function (e, n) {
+      var t = new XMLHttpRequest();
+      return (
+        'withCredentials' in t
+          ? t.open(e, n, !0)
+          : 'undefined' != typeof XDomainRequest
+          ? (t = new XDomainRequest()).open(e, n)
+          : (t = null),
+        t
+      );
+    },
+  };
 
-    // SecureToken: '3fa6018a-b074-4d91-98e6-4274c07e6c41',
+  Email.send({
+    // Host: 'smtp.elasticemail.com',
+    // Username: 'jsosa@globaluy.com',
+    // Password: 'Javiviso1583',
+
+    SecureToken: 'c17b5ee4-0e47-4812-816f-c83b7ebd073c',
     To: 'jcarlos.sosagomez@gmail.com',
-    From: `${email}`,
+    From: 'jcarlos.sosagomez@gmail.com',
     Subject: `You have a new message`,
-    Body: `${message}`,
+    Body: `Sender: ${email} <br> Message: ${message}`,
   }).then((message) => alert('Mail sent successfully!'));
 
   document.getElementById('email').value = '';
@@ -43,3 +83,5 @@ function sendEmail(e) {
 
 //   sendEmail(email, message);
 // }
+
+// SMTP Elastic email password: 5F31796581FF4B644AEBADF068773C874B29
